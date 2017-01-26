d8ee949980ba2314503b6609e00ed4d9f3fefaaf
Maxime Mouchet <max@maxmouchet.com>
2017-01-26 17:33:02 +0100

    Fix fox macOS

--- mu.sh
+++ mu.sh
@@ -5,8 +5,8 @@ nb_reads=`cat $1 | head -n 2 | tail -n 1`
 dna_size=`cat ~/tests/1  | head -n 1 | wc -c`
 ##echo $nb_reads
 
-sed -i  "s/#define N_READS .*/#define N_READS $nb_reads/"  ./rosalind.ez
-sed -i "s/#define DNA_SIZE .*/#define DNA_SIZE $dna_size/" ./rosalind.ez
+sed -i.bak "s/#define N_READS .*/#define N_READS $nb_reads/"  ./rosalind.ez
+sed -i.bak "s/#define DNA_SIZE .*/#define DNA_SIZE $dna_size/" ./rosalind.ez
 sh pi.sh rosalind.ez
 
 ./rosalind $1
--- pi.sh
+++ pi.sh
@@ -4,8 +4,17 @@ make clean
 
 easea $1
 
-N_THREADS=$((`nproc` * 2))
+if [[ `uname` == 'Darwin' ]]; then
+  cp Makefile.macos Makefile
+fi
+
+if [[ `uname` == 'Darwin' ]]; then
+  N_THREADS=$((`sysctl -n hw.ncpu` * 2))
+else
+  N_THREADS=$((`nproc` * 2))
+fi
+
 echo "Using $N_THREADS threads for compilation"
 
-sed -i 's/globalRandomGenerator->random_shuffle/random_shuffle/g' rosalindIndividual.cpp
+sed -i.bak 's/globalRandomGenerator->random_shuffle/random_shuffle/g' rosalindIndividual.cpp
 make -j $N_THREADS
